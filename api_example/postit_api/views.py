e0868d29a2b5d06669953c1c988a0b1a97f09fae
DonatasNoreika <donoras@gmail.com>
2026-02-19 09:00:51 +0200

    Galima ir kurti postus tik prisijungus

--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .serializers import PostSerializer
 from .models import Post, Comment, PostLike, CommentLike
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
 
